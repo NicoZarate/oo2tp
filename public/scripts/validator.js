edc72fe221b90e4b4f5c6c54a5b8815dc0d47237
Gabriela Yael Rodriguez <gabrielayaelrodriguez@gmail.com>
2017-03-20 21:23:11 -0300

    actualizar listo!

--- public/scripts/validator.js
+++ public/scripts/validator.js
@@ -69,3 +69,40 @@ jQuery(function ($) {
         }
     });
 });
+
+jQuery(function ($) {
+	$('#upform').validate({
+        rules: {
+            nombreUp: {
+                required: true
+            },
+            tran1Up: {
+                required: true
+            },
+            tran2Up: {
+                required: true
+            },
+            startUp: {
+                required: true
+            },
+            endUp: {
+                required: true
+            }
+        },
+        highlight: function(element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+            if (element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
+});
